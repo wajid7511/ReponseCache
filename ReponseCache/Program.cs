b5ee47fc1c7c74dc380ee861afa11fe61cb657f9
wajidmuhammadnazar <email2wajidkhan@gmail.com>
2023-04-30 16:08:09 +0400

    first commit

---
+++ ReponseCache/Program.cs
@@ -0,0 +1,31 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//Respone Cache
+builder.Services.AddResponseCaching();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// should be after authorization to allow only authenticate users 
+app.UseResponseCaching();
+
+app.MapControllers();
+
+app.Run();
+
